4b904848d171695333802962a0bea8feee2376d5
Jinn <vz.jinn@gmail.com>
2022-11-24 12:03:27 +0900

    for Test

--- stu_Cpp/basic_point/new/class_t.cc
+++ stu_Cpp/basic_point/new/class_t.cc
@@ -3,10 +3,84 @@ using namespace std;
 
 class A{
 public:
-    A();
-    A();
+    //Constructor
+    A(){}
+    A(int a, int b):
+        _a(a),
+        _b(b)
+    {}
+
+//    //copy initialization, 就是构造函数的重载
+//    a(const A& a) {
+//        cout << "Copy initialization is running" << endl;
+//        _a = a._a;
+//        _b = a._b;
+//    }
+//
+//    //重载赋值运算符函数
+//    a& operator = (const A& a) {
+//        cout << "重载赋值运算符函数" << endl;
+//        _a = a._a;
+//        _b = a._b;
+//        return *this;
+//    }
+//    
+    void show(){
+        cout << "_a = " << _a << endl;
+        cout << "_b = " << _b << endl;
+    }
+
+    int _a = 1;
+    int _b = 11;
+
+    
 };
 
 int main (void) {
+    cout << "a1, a2 has constructed" << endl;
+    A a1(1,11),a2(2,22);
+    cout << "a1.show is " << endl;
+        a1.show();
+        cout << endl;
 
+    cout << "a2.show is " << endl;
+        a2.show();
+
+        cout << endl;
+
+    cout << "---------" << endl;
+
+    cout << "running \"A a3 = a2\" " << endl;
+     A a3 = a2;
+
+    cout << "expect a3 is same to a2" << endl;
+    cout << "a3.show is " << endl;
+        a3.show();
+
+        cout << endl;
+
+    cout << "a2.show is " << endl;
+        a2.show();
+
+        cout << endl;
+
+    cout << "a1.show is " << endl;
+        a1.show();
+        cout << endl;
+
+    cout << "========" << endl;
+
+    cout << "running \"a3 = a2\"  " << endl;
+        a3 = a2;
+        cout << endl;
+
+    cout << "a3.show is " << endl;
+        a3.show();
+
+        cout << endl;
+
+    cout << "a2.show is " << endl;
+        a2.show();
+
+    return 0;
 }
