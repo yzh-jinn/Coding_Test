b01ddeaf9f4da4f9e84806a9d9cf20396f3d5f38
Jinn <vz.jinn@gmail.com>
2022-11-29 14:50:17 +0800

    test

--- stu_Cpp/basic_point/new/hw.cc
+++ stu_Cpp/basic_point/new/hw.cc
@@ -1,20 +1,48 @@
 #include<iostream>
-#include<stdio.h>
+#include<string>
+
 using namespace std;
 
-//这里是地址,所以a[]可以是空的（因为不传长度）
-void arr(int a[]){ 
-    int i = 0;
-    for (i = 0; i<5; i++) {
-        printf("%d\n",a[i]);
+class A {
+public:
+    A(){}
+    A(int age, char* name):
+       _age(age),
+        _name(name)
+    {
+        cout << "Constructing" << endl;
     }
-}
+
+    void show() {
+        cout << _age << endl;
+        cout << _name << endl;
+    }
+
+    void changeWord(){
+ 
+        cout << "Input your Word : " << endl;
+        //char *new_word = nullptr;
+        //cin >> new_word;
+
+        strcpy_s(_name, "Good");
+        
+        cout << "This is your new Word : " << _name << endl;
+
+        //return *this;
+     }
+
+private:
+    int _age = 10;
+    char* _name = nullptr; //(char*)malloc(sizeof(char)); 
+};
+
 
 int main (void) {
-    int a[] = {10,20,30,40,50};
-    arr(a);
-    printf("%lu\n",sizeof(a)/sizeof(int));
+    A a(11, "Happy Good Day!");
+    a.show();
+    cout << "-------" << endl;
+    a.changeWord();
+    a.show();
+
     return 0;
 }
-
-
