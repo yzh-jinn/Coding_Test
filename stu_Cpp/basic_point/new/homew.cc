cd46252e73d19c560b000d1dcfce3d65883a9745
Jinn <vz.jinn@gmail.com>
2022-11-29 14:00:23 +0900

    for Test

---
+++ stu_Cpp/basic_point/new/homew.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+//这里是地址,所以a[]可以是空的（因为不传长度）
+void arr(int a[]){ 
+    int i = 0;
+    for (i = 0; i<5; i++) {
+        printf("%d\n",a[i]);
+    }
+}
+
+int main (void) {
+
+    int a[] = {10,20,30,40,50};
+    arr(a);
+    printf("%lu\n",sizeof(a)/sizeof(int));
+    return 0;
+}
+
+
