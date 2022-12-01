deaca74edb278a4e2368e9d3c4344333a89025ef
Jinn <vz.jinn@gmail.com>
2022-12-01 22:55:12 +0900

    for Test

--- stu_Cpp/basic_point/new/hw.cc
+++ stu_Cpp/basic_point/new/hw.cc
@@ -1,49 +1,28 @@
 #include<iostream>
-#include<string>
-
 using namespace std;
 
-class A {
+class A{
 public:
     A(){}
-    A(int age, char* name):
-       _age(age),
+    A(const char* name):
         _name(name)
-    {
-        cout << "Constructing" << endl;
+    {}
+
+    void show() const {
+        cout << "Name is : " << _name << endl;
     }
 
-    void show() {
-        cout << _age << endl;
-        cout << _name << endl;
-    }
-
-    void changeWords(char* changeWords){
- 
-        cout << "Input your Word : " << endl;
-        //char *new_word = nullptr;
-        cin >> changeWords;
-
-        strcpy(_name, changeWords);
-        
-        cout << "This is your new Word : " << _name << endl;
-
-        //return *this;
-     }
-
-private:
-    char* _name = nullptr; //(char*)malloc(sizeof(char))
-    int _age = 10;
+protected:
+    const char* _name = "Mike";
 };
 
-
 int main (void) {
-    A a(11, "Happy Good Day!");
-    a.show();
-    cout << "-------" << endl;
-    char* changeWords = "GoodBoy";
-    a.changeWords(changeWords);
-    a.show();
+
+    // A* a = new A[3]{"James","Bob","Marry"};
+    A (*a)[3] = new A[4][3]{"James","Bob","Marry","A1","A2","A3"};
+
+    (*a)[6].show();
+
 
     return 0;
 }
