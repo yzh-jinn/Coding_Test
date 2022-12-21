5eb6211744c5b97be95b0f6b3447e1aa86d7c3cd
Jinn <vz.jinn@gmail.com>
2022-12-21 20:00:35 +0800

    test

--- t1.c
+++ t1.c
@@ -1,1 +1,21 @@
-#include<test>
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+
+typedef struct test{
+    int *a;
+    char *b;
+}test;
+
+void initTest(test *t){
+    t->a = (int*)malloc(SIZE*sizeof(int));
+    t->b = (char*)malloc(SIZE*sizeof(char));
+    //return *t;
+}
+
+int main (void) {
+    test *t1 = (test*)calloc(SIZE, sizeof(test));
+    initTest(t1);
+
+    return 0;
+}
