2c8e0a7dffa109c765310878984b35c75df6bad4
jinn <vz.jinn@gmail.com>
2023-01-12 07:11:17 +0900

    30days_test

---
+++ day30_C_DS/day1-12/doubleCircularLinkedList.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int dC_type;
+
+//创建双向循环链表
+typedef struct dCirLL{
+    struct dCirLL *prev;
+    dC_type data;
+    struct dCirLL *next;
+}dcLink;
+
+//
+
+int main (void) {
+    
+    return 0;
+}
