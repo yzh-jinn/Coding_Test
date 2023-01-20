16c0e000d35611a2881e4e21ff02964a94536c6d
Jinn <jinn@Jinns-MacBook-Pro.local>
2023-01-20 15:16:57 +0900

    30days_test

---
+++ rust_t/hello_world/target/debug/deps/hello_world-b0ccdc06c5b93d17.d
@@ -0,0 +1,5 @@
+/Users/jinn/coding_jinn_test/rust_t/hello_world/target/debug/deps/hello_world-b0ccdc06c5b93d17: src/main.rs
+
+/Users/jinn/coding_jinn_test/rust_t/hello_world/target/debug/deps/hello_world-b0ccdc06c5b93d17.d: src/main.rs
+
+src/main.rs:
