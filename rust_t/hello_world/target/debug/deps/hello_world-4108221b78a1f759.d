35f8c642e814e1fe4484216819603bbf708fe224
root <root@Silver_J>
2023-02-04 16:28:02 +0900

    test30days

---
+++ rust_t/hello_world/target/debug/deps/hello_world-4108221b78a1f759.d
@@ -0,0 +1,5 @@
+/root/coding_jinn_test/rust_t/hello_world/target/debug/deps/hello_world-4108221b78a1f759: src/main.rs
+
+/root/coding_jinn_test/rust_t/hello_world/target/debug/deps/hello_world-4108221b78a1f759.d: src/main.rs
+
+src/main.rs:
--- rust_t/hello_world/target/debug/hello_world.d
+++ rust_t/hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/Users/jinn/coding_jinn_test/rust_t/hello_world/target/debug/hello_world: /Users/jinn/coding_jinn_test/rust_t/hello_world/src/main.rs
+/root/coding_jinn_test/rust_t/hello_world/target/debug/hello_world: /root/coding_jinn_test/rust_t/hello_world/src/main.rs
