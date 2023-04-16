3d6c2b92ebbee593df72f3be026c1e70105a6b1c
Jinn <jinn@EmmysMacA.local>
2023-04-16 10:35:18 +0900

    add something on .vscode

--- rust_t/r_test/target/debug/deps/r_test-578bbbea31587211.d
+++
@@ -1,5 +0,0 @@
-/Users/jinn/coding_jinn_test/rust_t/r_test/target/debug/deps/r_test-578bbbea31587211: src/main.rs
-
-/Users/jinn/coding_jinn_test/rust_t/r_test/target/debug/deps/r_test-578bbbea31587211.d: src/main.rs
-
-src/main.rs:
--- rust_t/r_test/target/debug/r_test.d
+++
@@ -1,1 +0,0 @@
-/Users/jinn/coding_jinn_test/rust_t/r_test/target/debug/r_test: /Users/jinn/coding_jinn_test/rust_t/r_test/src/main.rs
