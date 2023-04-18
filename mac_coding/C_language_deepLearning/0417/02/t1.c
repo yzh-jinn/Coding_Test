9678a75b2c5d6a9368db2962cd038f846de7b606
Jinn <jinn@mac.local>
2023-04-18 09:06:59 +0900

    added new file on new

---
+++ mac_coding/C_language_deepLearning/0417/02/t1.c
@@ -0,0 +1,8 @@
+#include"t.h"
+
+int main (void) {
+    int x, X;
+    printf("go %d \n", x);
+
+    return 0;
+}
---
+++ mac_coding/C_language_deepLearning/0417/02/t2.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main (void) {
+    
+    
+    double i = 3.123456789;
+    std::cout << i << std::endl;
+    
+    int g = (int)i;
+    std::cout << g << std::endl;
+
+    return 0;
+}
