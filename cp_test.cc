9f109f1e6705bb5a2cf6593da5aeb096232b9884
jinn_air <vz.jinn@gmail.com>
2023-04-21 20:58:12 +0900

    updated

---
+++ cp_test.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+
+// void func(unsigned int a, int b) {
+
+// 	printf("you inputed : %u , %u", a, b);
+
+// }
+
+// int main (void) {
+
+//     unsigned int a = -10;
+// 	int b = 20;
+	
+// 	func(a, b);
+	
+
+//     return 0 ;
+// }
+
+void func () {
+
+	unsigned int a;
+	for (a = 9; a >= 0; a--){
+		printf("%d\n", a);
+	}
+
+}
+
+int main (void) {
+	func();
+	return 0;
+}
