aee8dbc4bd27ef46565a1b0d37d6630b6c0387b3
jinn_macp <vz.jinn@gmail.com>
2023-04-22 10:43:24 +0900

    update

--- cp_test.cc
+++ cp_test.cc
@@ -1,9 +1,19 @@
 #include<iostream>
-
+#include<float.h>
+#include<math.h>
 
 
 int main (void) {
+	
+	double x = 0.0;
 
+	if (fabs(x) < DBL_EPSILON ) {
+	    printf("this is true result!");
+	}
+	else {
+		printf("oooops!");
+	}
 
 	return 0;
 }
+
