ea2a9fb471e7a864b59351c803186ce0087c4407
jinn_macp <vz.jinn@gmail.com>
2023-04-22 09:35:30 +0900

    updated

--- cp_test.cc
+++ cp_test.cc
@@ -2,35 +2,8 @@
 
 
 
-
-// void func(unsigned int a, int b) {
-
-// 	printf("you inputed : %u , %u", a, b);
-
-// }
-
-// int main (void) {˜
-//     unsigned int a = -10;
-// 	int b = 20;
-	
-// 	func(a, b);
-	
-
-//     return 0 ;
-// }
-
-void func () {
-
-	unsigned int b = -20u;
-	for (b = 9; b >= 0; b--){
-		printf("%d\n", b);
-	}
-
-}
-
 int main (void) {
-	func();
-	const char *str = "hello";
-	
+
+
 	return 0;
 }
