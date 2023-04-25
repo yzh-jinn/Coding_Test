274ced2d3ca8cb5b56c1a35e0da71687f05b8f89
jinn_macp <vz.jinn@gmail.com>
2023-04-25 16:36:00 +0900

    update

--- test.cc
+++ test.cc
@@ -1,47 +1,19 @@
 #include<stdio.h>
-
-
-
+#include<unistd.h>
 
 int main (void) {
-	
-	printf("Input sth: \n");
-//	int m = 0;
-	char s = NULL;
-//	int count = 0;
-	int quit = 0;
-	while(!quit) {
-		int ret = scanf("%c", &s);
-		if(ret !=1 ) {
-			printf("invalid!\n");
-			int c;
-            while((c = getchar()) != '\n' && c != EOF);
-            continue;
+
+	while(1) {
+		int a = getchar();
+		if(a == '#') {
+//			break;
+			continue;
 		}
-		switch(s){
-			case 'A':
-				printf("day %c\n", s);
-				break;
-			case 'B':
-				printf("day %c\n", s);
-				break;
-			case 'C':
-				static int count = 0;
-				count++;
-				if(count >= 5){
-					printf("are you kidding me!\n");
-//					return 0;
-					quit = 1;
-				}
-				printf("input again\n");
-				break;
-			default:
-				printf("you wrong!\n");
-				break;
-		}
+		putchar(a);
+		printf("\n");
 	}
-	printf("run it again!\n");
-	
+	printf("\nbreak out!\n");
+
 	return 0;
 
 }
