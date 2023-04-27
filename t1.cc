e754fc38c321f06c8bd698c99466ae0b8d85daa5
jinn_macp <vz.jinn@gmail.com>
2023-04-27 16:37:22 +0900

    updated

--- t1.cc
+++ t1.cc
@@ -3,18 +3,9 @@
 
 int main (void) {
 
-	while(1) {
-		printf("input sth: ");
-		int a = getchar();
-		if(a == '#') {
-			break;
-//			continue;
-		}
-		putchar(a);
-		printf("\n");
-	}
-	printf("\nbreak out!\n");
-	
+	char *p = "hello\n";
+	*p = 'h';
+
 	return 0;
 
-}
+} 
