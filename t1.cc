4e6caab044806e8a231bfc979bb3a08b8d94382d
jinn_macp <vz.jinn@gmail.com>
2023-05-01 14:11:01 +0900

    updated

--- t1.cc
+++ t1.cc
@@ -5,10 +5,12 @@ typedef int arr[10];
 
 
 int main (void) {
-	arr b = { 1, 2, 4, 5, 1, 2, 4, 1, 2, 99};
-	for (int i = 0; i < 10; i++) {
-		printf("print:%d\n",b[i]);
-	}
+	//arr b = { 1, 2, 4, 5, 1, 2, 4, 1, 2, 99};
+	//for (int i = 0; i < 10; i++) {
+	//	printf("print:%d\n",b[i]);
+	//}
+	char t = 65;
+	printf("%c\n", t);
 
 	return 0;
 }
