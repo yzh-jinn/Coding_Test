113c7fb44a6e47770ccd59fdd8991cc218f8722a
jinn_macp <vz.jinn@gmail.com>
2023-05-02 17:23:03 +0900

    update

--- t1.cc
+++ t1.cc
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
+int show () {
+	printf("worked!\n");
+	return 1;
+}
 
-int main (void) {
 
+ int main (void) {
+ 	char a = 'A';
+ 	char b = 0;
+ 	printf("%d\n",a);
+ 	printf("%d\n",b);
 
 	return 0;
 }
