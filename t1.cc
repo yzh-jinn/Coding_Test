57812ecc947879218e496a8b0434e6574a75f35d
jinn_macp <vz.jinn@gmail.com>
2023-05-03 17:55:42 +0900

    update

--- t1.cc
+++ t1.cc
@@ -1,8 +1,13 @@
 #include "fun.h"
 
 int main (void) {
-	int a = 0xFFFFFFFF;
-	printf("%d\n",a);
-	showBits(a);	
+	int a = 10;
+	int b = a++;
+	printf("a: %d\nb: %d\n",a,b);
+
+	int c = 0xDD;
+	printf("before c: %d\n",c);
+	c++;
+	printf("c: %d\n",c);
 	return 0;
 }
