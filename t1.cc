d090e4574e5836f4b6a0c2515105515836f523ae
jinn_macp <vz.jinn@gmail.com>
2023-05-04 08:32:28 +0900

    updated

--- t1.cc
+++ t1.cc
@@ -1,13 +1,15 @@
 #include "fun.h"
+#include<math.h>
 
 int main (void) {
 	int a = 10;
 	int b = a++;
 	printf("a: %d\nb: %d\n",a,b);
-
+	
 	int c = 0xDD;
 	printf("before c: %d\n",c);
 	c++;
 	printf("c: %d\n",c);
+
 	return 0;
 }
