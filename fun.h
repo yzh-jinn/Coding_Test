1a39b3c178db696e4b714fbfd25b11bd350a440a
jinn_macp <vz.jinn@gmail.com>
2023-05-06 10:14:50 +0900

    updated

--- fun.h
+++ fun.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<stdio.h>
 #include<math.h>
+#include<iostream>
 #define CAL(x,y)  (((y) == 0 ? 0 : ( (x) % (y))) )
 #define CODE(x,y) do{ \
 	printf("%d\n",(x)); \
@@ -9,7 +10,6 @@
 	}\
 }while(0)
 #define VERSION 0
-
+#define show(x) (std::cout << (x) << std::endl)
 
 void showBits(int x);
-
--- main.cc
+++ main.cc
@@ -1,17 +1,14 @@
 #include "fun.h"
 
-int x = 20;
 
 int main (void) {
-	int * p = NULL;
-	printf("1. &p = %p\n", (void*)&p);
-	p = (int*)&p; //将 p 的地址放入 p 变量（自己指自己）
-	printf("2. p = (int *)&p = ? %p\n", p);
-	*p = 10; //将 p 地址指向的空间内容改为10
-	//p = 20;
-	printf("3. *p = 10, &p = ? %p\n", p);
-//	printf("4. *p = 10, *p = ? %d",*p);
-
+	
+	int a = 10;
+	int *p = NULL;
+	show(p);
+	show(p+1);
+	int * *p2 = 
+	show(p2);
 
 
 	return 0;
