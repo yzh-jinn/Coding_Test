da134fd3daa2e998a65506605c4a664bbbc285c3
jinn_macp <vz.jinn@gmail.com>
2023-05-09 18:02:36 +0900

    update

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 
 int main (void) {
 	int a[4] = {1,2,3,4};
-	int *ptr = (int *)((int)a + 1);
-	printf("%p")
+//	int *ptr = (int *)((int)a + 1);
+	printf("%p\n",(int)a);
+	printf("%d\n",(int)a);
+	printf("%p\n",(int)a+1);
+	printf("%d\n",(int)a+1);
 	
 	return 0;
 
