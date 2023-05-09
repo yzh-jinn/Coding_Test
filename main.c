a8bbaf2cbcb445e81cf18f8f4780accf8f59d70b
jinn_macp <vz.jinn@gmail.com>
2023-05-09 17:02:49 +0900

    updated

---
+++ main.c
@@ -0,0 +1,16 @@
+//#include "fun.h"
+#include<stdio.h>
+
+
+int main (void) {
+	int a[4] = {1,2,3,4};
+	int *ptr = (int *)((int)a + 1);
+	printf("a %%p: %p\n",a); 
+	printf("(int)a %%p: %p\n",(int)a);
+	printf("(int)a %%d: %d\n",(int)a);
+	printf("%x\n",*ptr);
+//	int *ptr2 = (int *)((int)a + 1);
+//	printf("%x, %x\n", ptr1[-1], *ptr2);
+	return 0;
+
+}
