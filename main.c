d361cf7bf029674c4bedc0b1aa45406705a68039
jinn_ssh <vz.jinn@gmail.com>
2023-05-12 16:19:22 +0900

    updated_centos

---
+++ main.c
@@ -0,0 +1,20 @@
+//#include "fun.h"
+#include<stdio.h>
+ 
+int main (void) {
+ 
+
+	int a = 10;
+	int *p = &a;
+	int **pp = &p; 
+//    printf("%p\n", p);
+//	p = 100;
+//    printf("%p\n", p);
+//    int *d = ;
+//    printf("%d\n", p);
+    printf("%d\n", pp);
+
+
+	return 0; 
+
+}
