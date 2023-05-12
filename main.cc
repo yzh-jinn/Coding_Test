c35749d9d7c9c79b2ea2110a0c6064c55dd10633
jinn_macp <vz.jinn@gmail.com>
2023-05-12 16:12:45 +0900

    update

--- main.cc
+++ main.cc
@@ -3,12 +3,14 @@
  
 int main (void) {
  
-	int a[5][5];
-	int (*p)[4];
-	
-	p = a;
-	printf("a_ptr = %p, p_ptr = %p\n", &a[4][2], &p[4][2]);
-	printf("%p, %d\n", &p[4][2] - &a[4][4], &p[4][2] - &a[4][2]);
+
+	int a = 10;
+	int *p = &a;
+	int **pp = &p; 
+
+	p = 100;
+
+
 
 	return 0; 
 
