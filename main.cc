fbf8c209cc6ae9a7d0a070bd2d3b2813df48cc36
jinn_macp <vz.jinn@gmail.com>
2023-05-29 17:19:58 +0900

    updated

--- main.cc
+++ main.cc
@@ -1,10 +1,10 @@
 #include "fun.h"
 //#include<stdio.h>
 //#include<stdlib.h>
-
+#include <mysql/mysql.h>
 
 int main (void) {
-
+	cout << "hello: " << mysql_get_client_info() << endl;
 
     return 0;
 
