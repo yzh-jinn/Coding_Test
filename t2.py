1dab7e9fca09a2e9f9c4affeaaee60fe210f2f9b
vz.jinn <vz.jinn@gmail.com>
2023-11-15 18:02:12 +0900

    updated

--- t2.py
+++ t2.py
@@ -1,8 +1,19 @@
-shop_list = ('tomato', 'onion')
-print(shop_list[1])
-print("hello")
-print("hl")
+import openai
 
+test key = ""
+model = "gpt-3.5-turbo"
+messages = 
+[
+    {"role" : "user", "content": "Translate my messages to Chinese:"},
+    {"role" : "user", "content": "This is a perfect person."}
+]
+
+client = openai.OpenAI(api key = test key)
+response = client.chat.completions.create(
+    model = model,
+    messages = messages)
+
+print(response.choices[0].message)
 
 
 
