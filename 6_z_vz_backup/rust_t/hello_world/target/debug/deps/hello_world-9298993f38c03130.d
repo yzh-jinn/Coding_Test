62e8cccb4560d348e7f22437655e48397b364330
vz.jinn <vz.jinn@gmail.com>
2023-11-16 00:33:57 +0900

    updated

---
+++ 6_z_vz_backup/rust_t/hello_world/target/debug/deps/hello_world-9298993f38c03130.d
@@ -0,0 +1,5 @@
+/home/jinn/coding_jinn_test/6_z_vz_backup/rust_t/hello_world/target/debug/deps/hello_world-9298993f38c03130: src/main.rs
+
+/home/jinn/coding_jinn_test/6_z_vz_backup/rust_t/hello_world/target/debug/deps/hello_world-9298993f38c03130.d: src/main.rs
+
+src/main.rs:
--- 6_z_vz_backup/rust_t/hello_world/target/debug/hello_world.d
+++ 6_z_vz_backup/rust_t/hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/root/coding_jinn_test/rust_t/hello_world/target/debug/hello_world: /root/coding_jinn_test/rust_t/hello_world/src/main.rs
+/home/jinn/coding_jinn_test/6_z_vz_backup/rust_t/hello_world/target/debug/hello_world: /home/jinn/coding_jinn_test/6_z_vz_backup/rust_t/hello_world/src/main.rs
