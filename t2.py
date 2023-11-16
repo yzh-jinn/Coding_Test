39f4769b49afa2f54c15f9f5ff42b0b4292f9b64
jinn_macp <vz.jinn@gmail.com>
2023-11-16 09:14:39 +0900

    updated

--- t2.py
+++ t2.py
@@ -2,13 +2,12 @@ import openai
 
 test key = ""
 model = "gpt-3.5-turbo"
-messages = 
-[
+messages = [
     {"role" : "user", "content": "Translate my messages to Chinese:"},
     {"role" : "user", "content": "This is a perfect person."}
 ]
 
-client = openai.OpenAI(api key = test key)
+client = openai.OpenAI(api_key = test key)
 response = client.chat.completions.create(
     model = model,
     messages = messages)
