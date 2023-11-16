c907897aec8a44cea8e51536241dc03643133915
vz.jinn <vz.jinn@gmail.com>
2023-11-16 14:08:22 +0900

    updated

--- t2.py
+++ t2.py
@@ -1,15 +1,2 @@
-import openai
-
-test_key = "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"
-model = "gpt-4-turbo"
-messages = [
-    {"role" : "user", "content": "Translate my messages to Chinese:"},
-    {"role" : "user", "content": "This is a perfect person."}
-]
-
-client = openai.OpenAI(api_key = test_key)
-response = client.chat.completions.create(
-    model = model,
-    messages = messages)
-
-print(response.choices[0].message)
+class Student:
+    press
