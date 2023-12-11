cb915b669754f428cd5c5e749db7d6a447d73175
jinn_macp <vz.jinn@gmail.com>
2023-12-11 17:36:28 +0900

    updated

--- t2.py
+++ t2.py
@@ -1,4 +1,11 @@
-def testADD(a:int , b:int):
-    return a + b
+# def testADD(a:int = 1, b:int = 3):
+#     return a + b
 
-print(testADD(10, 39))
+# print(testADD())
+
+def testADD(a:int = 10, b:int = 20):
+    return a, a+b
+
+a, b = testADD(38, 90)
+
+print(a, b)
