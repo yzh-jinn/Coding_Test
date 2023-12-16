560b6c07ad32261e9c0fd2f6dfbd41b83d4930a4
jinn_macp <vz.jinn@gmail.com>
2023-12-16 20:21:19 +0900

    updated

--- t2.py
+++ t2.py
@@ -1,22 +1,5 @@
-# new_dict = {key_expression: value_expression for item in iterable if conditions}
+my_list = ["mike", "jack", "jack"]
 
-# key_expression: value_expression     # 新字典的格式
-# item   # key:value
-# iterable  # 筛选条件，比如： student.items
+new_list = frozenset(my_list)
 
-# 举例
-
-grades = {
-    "Bob" : 40,
-    "Mike" : 70,
-    "Emmy" : 120
-}
-
-new_grades = {
-    name: grade
-    for name, grade
-    in grades.items()
-    if grade >= 60
-}
-
-print(new_grades)
+print(new_list)
