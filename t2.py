f7e22537bc8518fc9180a407dea31f583e047458
jinn_macp <vz.jinn@gmail.com>
2023-12-18 11:52:23 +0900

    update

--- t2.py
+++ t2.py
@@ -1,6 +1,34 @@
-import os
+import csv
 
-with open("new_py.py", "w") as ob1:
-    ob1.write("hello this is my first time to create a new file which names 'new_py.py'")
+with open("newfile.csv",'w') as file:
 
-os.rename("new_py.py", "new_py")
+    csv_header = ["Name", "Chinese", "English", "Math"]
+
+    csv_writer = csv.DictWriter(file, csv_header)
+
+    rows = [
+        {
+            "Name": "Mike",
+            "Chinese" : 90,
+            "English" : 100,
+            "Math" : 120
+        },
+        {
+            "Name": "Bob",
+            "Chinese" : 89,
+            "English" : 66,
+            "Math" : 89
+        },
+        {
+            "Name": "Emmy",
+            "Chinese" : 60,
+            "English" : 80,
+            "Math" : 100
+        }
+    ]
+
+    csv_writer.writeheader()
+    csv_writer.writerows(rows)
+    
+if __name__ == '__main__' :
+    filename = "newfile.csv"
