19a9dd7eb6e51e232a4631f3d0f249516ee97db9
jinn_macp <vz.jinn@gmail.com>
2024-01-13 23:05:53 +0900

    updated

--- t2.py
+++ t2.py
@@ -1,31 +1,35 @@
-def calculate(operation: str, n1: int, n2: int):
-    def add(n1, n2):
-        return n1 + n2
+class Stduent:
+    pass
 
-    def sub(n1, n2):
-        return n1 - n2
 
-    def div(n1, n2):
-        if n2 == 0:
-            return "Division by zero is not allowed"
-        return n1 / n2
+# def calculate(operation: str, n1: int, n2: int):
+#     def add(n1, n2):
+#         return n1 + n2
 
-    def mul(n1, n2):
-        return n1 * n2
+#     def sub(n1, n2):
+#         return n1 - n2
 
-    if operation == 'add':
-        return add(n1, n2)
-    elif operation == 'sub':
-        return sub(n1, n2)
-    elif operation == 'div':
-        return div(n1, n2)
-    elif operation == 'mul':
-        return mul(n1, n2)
-    else:
-        return "Invalid operation"
+#     def div(n1, n2):
+#         if n2 == 0:
+#             return "Division by zero is not allowed"
+#         return n1 / n2
+
+#     def mul(n1, n2):
+#         return n1 * n2
+
+#     if operation == 'add':
+#         return add(n1, n2)
+#     elif operation == 'sub':
+#         return sub(n1, n2)
+#     elif operation == 'div':
+#         return div(n1, n2)
+#     elif operation == 'mul':
+#         return mul(n1, n2)
+#     else:
+#         return "Invalid operation"
     
 
-# f1 = calculate('add', 1, 2)
-f2 = calculate()
+# # f1 = calculate('add', 1, 2)
+# f2 = calculate()
 
-f2('sub', 2, 1)
+# f2('sub', 2, 1)
