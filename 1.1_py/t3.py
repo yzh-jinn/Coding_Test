3c92ab6aebce71758b009e11351c5d7b3d7fd6bd
jinn_macp <vz.jinn@gmail.com>
2024-01-21 10:59:38 +0900

    updated

--- 1.1_py/t3.py
+++ 1.1_py/t3.py
@@ -1,17 +1,2 @@
 print("hello")
-
-class Student:
-    pass
-
-std_1 = Student()
-
-def main():
-    a: int = 222
-    b: float = 11.222
-
-    print(type(a+b))
-
-
-if __name__ == '__main__':
-    main()
-
+print("good boy")
