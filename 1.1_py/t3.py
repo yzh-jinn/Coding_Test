8f0148fdbd9835ebd996ed3d23946b12c513fd54
jinn_macp <vz.jinn@gmail.com>
2024-02-27 11:55:32 +0900

    updated

--- 1.1_py/t3.py
+++ 1.1_py/t3.py
@@ -1,2 +1,19 @@
-print("hello")
-print("good boy")
+from enum import Enum
+
+@Enum.unique
+class Status(Enum):
+    SUCCESS = 1
+    OK = 2
+    FAIL = 3
+    WRONG = 4
+
+def main():
+    for s in Status:
+        print(s.name)
+
+    print(Status.__members__)
+
+    print(Status.SUCCESS == Status.OK)
+
+if __name__ == "__main__":
+    main()
